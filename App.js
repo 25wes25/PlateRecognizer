b32b4bae8b195143e96b12e9b4c5fcd9cdbf2eae
Wesley <wesleyswanson97@gmail.com>
2019-03-02 22:52:16 -0800

    Compiling and asking for camera access.

--- App.js
+++ App.js
@@ -1,50 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    StatusBar,
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+import Camera from 'react-native-openalpr';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+    },
+    textContainer: {
+        position: 'absolute',
+        top: 100,
+        left: 50,
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 20,
+    },
 });
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.camera = null;
+        this.state = {
+            camera: {
+                aspect: Camera.constants.Aspect.fill,
+            },
+            plate: 'Scan a plate',
+        };
+    }
+
+    onPlateRecognized = ({ plate, confidence }) => {
+        if (confidence > 90) {
+            this.setState({
+                plate,
+            })
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={this.state.camera.aspect}
+                    captureQuality={Camera.constants.CaptureQuality.medium}
+                    country="us"
+                    onPlateRecognized={this.onPlateRecognized}
+                    plateOutlineColor="#ff0000"
+                    showPlateOutline
+                    torchMode={Camera.constants.TorchMode.off}
+                    touchToFocus
+                />
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>{this.state.plate}</Text>
+                </View>
+            </View>
+
+        );
+    }
+}
+
+AppRegistry.registerComponent('PlateRecognizer', () => App);
--- android/app/src/main/java/com/platerecognizer/MainApplication.java
+++ android/app/src/main/java/com/platerecognizer/MainApplication.java
@@ -3,6 +3,7 @@ package com.platerecognizer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sandro.openalpr.CameraReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new CameraReactPackage()
       );
     }
 
